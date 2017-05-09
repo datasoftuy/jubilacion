de9e81cc7518dcb06193ca1580a2baad66c86415
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-09 11:00:44 -0300

    minisculas

---
+++ jubilacion/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace jubilacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int edad;
+            string genero;
+
+
+            Console.Write("Ingrese la edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Sexo solo minusculas m/f: ");
+            genero = Console.ReadLine();
+
+
+
+            if (genero == "m" && edad > 65)
+            {
+
+                Console.Write("Se puede jubilar");
+            }
+
+
+            else if (genero == "m" && edad < 65)
+            {
+
+                Console.Write("No se puede Jubilar");
+            }
+
+
+            else if (genero == "f" && edad > 60)
+            {
+
+                Console.Write("Se puede jubilar");
+
+            }
+
+
+            else
+            {
+                Console.Write("No se puede Jubilar");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
